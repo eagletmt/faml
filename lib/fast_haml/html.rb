a802bd22e947fc7d3620ca4a5ede05a9a84ea2cd
Kohei Suzuki <eagletmt@gmail.com>
2015-01-27 21:28:41 +0900

    Skip attribute builder generation if attrs is empty

--- lib/fast_haml/html.rb
+++ lib/fast_haml/html.rb
@@ -3,12 +3,16 @@ require 'fast_haml/attribute_builder'
 module FastHaml
   class Html < Temple::HTML::Fast
     def on_html_attrs(*attrs)
-      @sym = unique_name
-      [:multi,
-       [:code, "#{@sym} = ::FastHaml::AttributeBuilder.new(#{options.to_h.inspect})"],
-       *attrs.map { |attr| compile(attr) },
-       [:dynamic, "#{@sym}.build"],
-      ]
+      if attrs.empty?
+        [:multi]
+      else
+        @sym = unique_name
+        [:multi,
+         [:code, "#{@sym} = ::FastHaml::AttributeBuilder.new(#{options.to_h.inspect})"],
+         *attrs.map { |attr| compile(attr) },
+           [:dynamic, "#{@sym}.build"],
+        ]
+      end
     end
 
     def on_haml_attr(code)
