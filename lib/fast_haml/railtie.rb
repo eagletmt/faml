012757c88e8180386f3d95ea3ae334a3f8915874
Kohei Suzuki <eagletmt@gmail.com>
2015-02-24 19:58:39 +0900

    Revert haml loading

--- lib/fast_haml/railtie.rb
+++ lib/fast_haml/railtie.rb
@@ -2,11 +2,6 @@ module FastHaml
   class Railtie < ::Rails::Railtie
     initializer :fast_haml do |app|
       require 'fast_haml/rails_handler'
-      begin
-        # Load Haml::Plugin earlier to overwrite template handler with fast_haml.
-        require 'haml/template'
-      rescue LoadError
-      end
       ActionView::Template.register_template_handler(:haml, FastHaml::RailsHandler.new)
     end
   end
