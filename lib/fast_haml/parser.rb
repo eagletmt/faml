bd79527778cf65c7320a8ed3820f63df6d56b377
Kohei Suzuki <eagletmt@gmail.com>
2015-02-27 23:49:32 +0900

    Fix empty line counting at parsing phase

--- lib/fast_haml/parser.rb
+++ lib/fast_haml/parser.rb
@@ -59,7 +59,7 @@ module FastHaml
       text, indent = @indent_tracker.process(line, @line_parser.lineno)
 
       if text.empty?
-        @ast.increment_trailing_empty_lines
+        @ast.children.last.increment_trailing_empty_lines
         return
       end
 
--- spec/compiler_newline_spec.rb
+++ spec/compiler_newline_spec.rb
@@ -43,4 +43,13 @@ HAML
   %span world
 HAML
   end
+
+  it do
+    expect { render_string(<<HAML) }.to raise_error(LineVerifier, raised_at(4))
+%div
+  %span= 1
+
+  %span= raise LineVerifier
+HAML
+  end
 end
