a2821d11505fa82c655f28a89ac6e672dab41bf2
Kohei Suzuki <eagletmt@gmail.com>
2015-03-10 20:01:36 +0900

    More fair benchmarking

--- benchmark/rendering.rb
+++ benchmark/rendering.rb
@@ -14,15 +14,21 @@ Benchmark.ips do |x|
   obj = Object.new
 
   Haml::Engine.new(File.read(template), ugly: true, escape_html: true).def_method(obj, :haml)
-  code = FastHaml::Engine.new.call(File.read(template))
-  obj.instance_eval("def fast_haml; #{code}; end")
+  code_array = FastHaml::Engine.new.call(File.read(template))
+  obj.instance_eval("def fast_haml_array; #{code_array}; end")
+  code_string = FastHaml::Engine.new(generator: Temple::Generators::RailsOutputBuffer).call(File.read(template))
+  obj.instance_eval("def fast_haml_string; #{code_string}; end")
 
-  x.report('Haml rendering') do
+  x.report('Haml') do
     obj.haml
   end
 
-  x.report('FastHaml rendering') do
-    obj.fast_haml
+  x.report('FastHaml (Array)') do
+    obj.fast_haml_array
+  end
+
+  x.report('FastHaml (String)') do
+    obj.fast_haml_string
   end
 
   x.compare!
