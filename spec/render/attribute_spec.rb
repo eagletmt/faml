eagletmt/faml

commit 2fc26c71aaebd91ac4bfe72ca61924fd988025c0

Support array literal optimization

lib/faml/compiler.rb
@@ -318,13 +318,23 @@ module Faml
       parser.static_attributes.each do |k, v|
         static_attributes[k.to_s] = v
       end
+
+      class_list = Array(static_attributes['class']).flat_map { |c| c.to_s.split(/ +/) }
       unless static_class.empty?
-        class_list = static_attributes.fetch('class', '').to_s.split(/ +/)
-        static_attributes['class'] = static_class.split(/ +/).concat(class_list).uniq.sort.join(' ')
+        class_list.concat(static_class.split(/ +/))
       end
+      unless class_list.empty?
+        static_attributes['class'] = class_list.uniq.sort.join(' ')
+      end
+
+      id_list = Array(static_attributes['id'])
       unless static_id.empty?
-        static_attributes['id'] = [static_id, static_attributes['id']].compact.join('_')
+        id_list = [static_id].concat(id_list)
       end
+      unless id_list.empty?
+        static_attributes['id'] = id_list.join('_')
+      end
+
       static_attributes
     end
 

spec/render/attribute_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe 'Attributes rendering', type: :render do
 
   it 'renders array class' do
     expect(render_string('%span.c2{class: "c1"}')).to eq("<span class='c1 c2'></span>\n")
-    expect(render_string('%span.c2{class: ["c1", "c3"]}')).to eq("<span class='c1 c2 c3'></span>\n")
+    expect(render_string('%span.c2{class: ["c1", "c3", :c2]}')).to eq("<span class='c1 c2 c3'></span>\n")
   end
 
   it 'renders boolean attributes' do
@@ -119,6 +119,7 @@ HAML
 
   it 'merges static id' do
     expect(render_string('#foo{id: "bar"} baz')).to eq("<div id='foo_bar'>baz</div>\n")
+    expect(render_string('#foo{id: %w[bar baz]} hoge')).to eq("<div id='foo_bar_baz'>hoge</div>\n")
   end
 
   it 'merges static class' do

lib/faml/static_hash_parser.rb
@@ -85,7 +85,8 @@ module Faml
         end
       when :hash
         try_static_hash_value(key_static, node)
-        # TODO: Add array case
+      when :array
+        try_static_array_value(key_static, node)
       else
         throw FAILURE_TAG
       end
@@ -112,6 +113,13 @@ module Faml
         end.join(', ')
         @dynamic_attributes[key_static] = "{#{expr}}"
       end
+    end
+
+    def try_static_array_value(key_static, node)
+      arr = node.children.map do |child|
+        try_static_value(key_static, child)
+      end
+      @static_attributes[key_static] = arr
     end
   end
 end

commit 4fb6ef5543871e5578c8b67d0c0baa8d7bed9151

Fix regression caused by 2fc26c71aaebd91ac4bfe72ca61924fd988025c0

lib/faml/compiler.rb
@@ -319,7 +319,7 @@ module Faml
         static_attributes[k.to_s] = v
       end
 
-      class_list = Array(static_attributes['class']).flat_map { |c| c.to_s.split(/ +/) }
+      class_list = Array(static_attributes.delete('class')).flat_map { |c| c.to_s.split(/ +/) }
       unless static_class.empty?
         class_list.concat(static_class.split(/ +/))
       end
@@ -327,7 +327,7 @@ module Faml
         static_attributes['class'] = class_list.uniq.sort.join(' ')
       end
 
-      id_list = Array(static_attributes['id'])
+      id_list = Array(static_attributes.delete('id'))
       unless static_id.empty?
         id_list = [static_id].concat(id_list)
       end

spec/render/attribute_spec.rb
@@ -64,8 +64,16 @@ HAML
     expect(render_string('%span.foo{class: %w[foo bar]}')).to eq("<span class='bar foo'></span>\n")
   end
 
+  it 'skips empty array class' do
+    expect(render_string('%span{class: []}')).to eq("<span></span>\n")
+  end
+
   it 'strigify non-string ids' do
     expect(render_string('%span#foo{id: :bar} hello')).to eq("<span id='foo_bar'>hello</span>\n")
+  end
+
+  it 'skips empty array class' do
+    expect(render_string('%span{id: []}')).to eq("<span></span>\n")
   end
 
   it 'escapes' do